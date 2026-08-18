57a3e88a19b8ea6421358d13a8fa97e0ab9a7868
Md Mominul Islam <mdmominulislam5600@gmail.com>
2026-08-18 22:54:32 +0600

    Add configuration file with API token and interval

---
+++ config.js
@@ -0,0 +1,8 @@
+/* ==========================================
+   CONFIGURATIONS & API KEYS
+========================================== */
+
+const CONFIG = {
+    SPORTMONKS_API_TOKEN: 'YOUR_ACTUAL_API_TOKEN', // এখানে আপনার আসল API টোকেন বসাবেন
+    REFRESH_INTERVAL: 30000 // ৩০ সেকেন্ড (মিলিসেকেন্ডে)
+};
